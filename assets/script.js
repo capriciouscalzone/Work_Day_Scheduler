4aad4d0d8a84f8e7c61744750b3095908eedd51b
Michael Crandall <mec192@gmail.com>
2022-09-19 13:11:52 -0500

    added storing time and text in local storage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
     $('#currentDay').text(moment().format("MMM Do YYY, h:mm:ss a"));
+
+$(".saveBtn").on("click", function() {
+    
+    console.log(this);
+    var text = $(this).siblings("description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+})
+
+
+
+
+
+
 })
