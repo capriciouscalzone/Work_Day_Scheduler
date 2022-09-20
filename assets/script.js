e34b13b32a43d82f54b32b184b4ae37eb137d74d
Michael Crandall <mec192@gmail.com>
2022-09-20 00:31:58 -0500

    writing a new time function and defining each hours timeblock

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+function baseCalendar () {
+    $(document).ready(function() {
+        let momentVar = moment().format('ddd, MMM Do, YYY h:mm a');
+        let workHours = ['9 a.m.', '10 a.m.','11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.']
+
+        function currentDay() {
+            let currentDay = $('#currentDay'). text(momentVar);
+            return currentDay;
+        }
+
+        
+    })
+}
